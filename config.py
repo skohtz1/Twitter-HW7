e9a5c5159c367fac860fcf7089e2af101c5ebfa0
unknown <gizmo104567@gmail.com>
2018-03-20 16:55:56 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "TY84chq5pwNzeCFXSqNLJtqPX"
+consumer_secret = "X62E3w9IQjmCYMk2qVSmrz226ympRLBLUZictLwfrScatuR5Js"
+access_token = "971511750586781696-5O1V7fn2GTJLRoalIMtTJibxWrI8zvl"
+access_token_secret = "3pioCSmSHTNCXZa6PMA7NmvK1RqGLJE7rIXk7BpVl0Joq"
